9ef237075786db70f3ab61ac9fa0b4f905efb61f
DieHard <kontra.bag@bk.ru>
2021-06-05 15:11:19 +0700

    start refract db api

--- db_agregation.js
+++ db_agregation.js
@@ -1,12 +1,46 @@
 const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('db.sqlite3');
 
-function getData(sql, callback) {
-    const db = new sqlite3.Database('db.sqlite3');
-    let data;
-    db.all(sql, (err, rows) => {
-        return callback(err, rows);
+const dbApi = {
+    dbConnect(sql, some = true) {
+
+    },
+
+    updateData() {
+
+    },
+}
+
+function dbConnect(sql, some = true) {
+    return new Promise((resolve, reject) => {
+        let data = [];
+        if (some) {
+            db.all(sql, (err, rows) => {
+                if (err) {
+                    return reject(err);
+                }
+                data = rows;
+                resolve(data);
+            });
+        } else {
+            db.get(sql, (err, rows) => {
+                if (err) {
+                    return reject(err);
+                }
+                data = rows;
+                resolve(data);
+            });
+        }
     });
-    db.close();
+}
+
+async function updateData() {
+
+}
+
+async function getData(sql, some) {
+    let data = await dbConnect(sql, some);
+    return data;
 }
 
 exports.db = getData;
--- routes/schedule.js
+++ routes/schedule.js
@@ -2,32 +2,15 @@ var express = require('express');
 var router = express.Router();
 let dbApi = require('../db_agregation')
 
-let teacherList;
-let lessonsList;
-
-
-dbApi.db('SELECT * FROM teachers', (err, rows) => {
-    if (rows !== undefined) {
-        teacherList = rows;
-    } else {
-        throw err;
-    }
-});
-
-dbApi.db('SELECT * FROM lessons', (err, rows) => {
-    if (rows !== undefined) {
-        lessonsList = rows;
-    } else {
-        throw err;
-    }
-});
-
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res, next) {
+    let teachers = await dbApi.db('SELECT * FROM teachers', true);
+    let lessons = await dbApi.db('SELECT * FROM lessons', true);
+
     res.render('schedule',{
         title: 'schedule',
-        teachersData: JSON.stringify(teacherList),
-        lessonsData: JSON.stringify(lessonsList),
+        teachersData: JSON.stringify(teachers),
+        lessonsData: JSON.stringify(lessons),
     });
 });
 
