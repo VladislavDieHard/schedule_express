0c658b49c2ed200d7d84e69e71c6bec01ff19599
Nixel <pechenkin685@gmail.com>
2021-08-18 22:26:39 +0700

    admin front in progress

---
+++ public/javascripts/adminDashFront/models.js
@@ -0,0 +1,10 @@
+// Модели для запроса гет тестовая часть
+let User = {
+    "token": "1c1a6b41994f10ef79233299e83f6470ea5d312664e8e63d97ae710069d02e11",
+    "method": "getAll",
+    "model": "User",
+    "where": {
+    "isAdmin": true
+    },
+    "includeModel": "School"
+}
