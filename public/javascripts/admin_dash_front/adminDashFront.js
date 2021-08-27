d7e64cc9ea098280e927c5f75b4bae61238bb75b
Nixel <pechenkin685@gmail.com>
2021-08-28 01:46:14 +0700

    add new update method for all model

--- public/javascripts/admin_dash_front/adminDashFront.js
+++ public/javascripts/admin_dash_front/adminDashFront.js
@@ -1,108 +1,89 @@
 const addData = {
-    async getDataUser(model, path){
-        let req  = table[model]
+    async getData(model){
+        let req  = models[model]
         let data = await req.get()
-        data.forEach(element => this.dataOutputUser(element, model))
+        data.forEach(element => addData.dataOutput(element, model))
     },
-    async getData(model, path){
-        let req  = table[model]
-        let data = await req.get()
-        data.forEach(element => this.dataOutput(element, model, path))
+    async updateData(id){
+        let model   = document.getElementById(`model`).value
+        let inputData = document.getElementById(`input-data${id}`).value
+        let req = models[model]
+        req.update(id, inputData)
+
+
     },
-    async update(model){
-        let req = table[model]
-        let data = await req.update()
+    async deleteData(){
+        if(document.getElementById('accordion')){
+            let contentItem = document.getElementById('content-item')
+            contentItem.remove()
+            let content = document.getElementById('content')
+            let child   = document.createElement('div')
+            child.id = 'content-item'
+            content.appendChild(child)
+        }
+
     },
-    async dataOutputUser(data, model){
-        let parent = document.getElementById('tab_01')
-        let element = document.createElement('button')
-        element.className = 'accordion'
-        element.onclick = accordionDown
-        element.id = data.id
-        element.innerHTML = (` <table>
-                                <tr>
-                                    <td>id</td>
-                                    <td><input disabled value='${data.id}'></td>
-                                    <td>логин</td>
-                                    <td><input disabled value='${data.login}'></td>
-                                    <td>Удален</td>
-                                    <td><input disabled value='${data.isDeleted}'></td>
-                                    <td>Модель</td>
-                                    <td><input disabled value='${model}'></td>
-                                </tr>
-                                <tr>
-                                    <td>Создан</td>
-                                    <td><input disabled value='${data.createdAt}'></td>
-                                    <td>Обновлен</td>
-                                    <td><input disabled value='${data.updatedAt}'></td>
-                                    <td>Школа</td>
-                                    <td><input disabled value='${data.SchoolId}'></td>
-                                </tr>
-                              </table>`)
-        parent.appendChild(element)
-        let elementCont = document.createElement('div')
-        elementCont.className = 'panel'
-        elementCont.innerHTML = (`<table>
-                                        <tr>
-                                            <td>Пароль</td>
-                                            <td><input id="password${data.id}" value='${data.login}'></td>
-                                            <td>Удалить</td>
-                                            <td><select id="status${data.id}" name="status[]" >
-                                                <option value='true'>Да</option>
-                                                <option value="false">Нет</option>
-                                            </select></td>
-                                        </tr>
-                                      </table>
-                                      <button onclick="addData.update(${model})">Update</button>`)
-        parent.appendChild(elementCont)
+    async accordionDown(){
+        let accordion = document.getElementsByClassName('accordion')
+        for(let i = 0; i < accordion.length; i++){
+            accordion[i].onclick = function () {
+                this.classList.toggle('active')
+                this.nextElementSibling.classList.toggle('show')
+            }
+        }
     },
-    async dataOutput(data, model, path){
-        let parent = document.getElementById(path)
-        let element = document.createElement('button')
-        element.className = 'accordion'
-        element.onclick = accordionDown
-        element.id = data.id
-        element.innerHTML = (` <table>
-                                <tr>
-                                    <td>id</td>
-                                    <td><input disabled value='${data.id}'></td>
-                                    <td>Имя</td>
-                                    <td><input disabled value='${data.name}'></td>
-                                    <td>Удален</td>
-                                    <td><input disabled value='${data.isDeleted}'></td>
-                                    <td>Модель</td>
-                                    <td><input disabled value='${model}'></td>
-                                </tr>
-                                <tr>
-                                    <td>Создан</td>
-                                    <td><input disabled value='${data.createdAt}'></td>
-                                    <td>Обновлен</td>
-                                    <td><input disabled value='${data.updatedAt}'></td>
-                                    <td>Школа</td>
-                                    <td><input disabled value='${data.SchoolId}'></td>
-                                </tr>
-                              </table>`)
-        parent.appendChild(element)
-        let elementCont = document.createElement('div')
-        elementCont.className = 'panel'
-        elementCont.innerHTML = (`<table>
-                                        <tr>
-                                            <td>Имя</td>
-                                            <td><input id="password${data.id}" value='${data.name}'></td>
-                                            <td>Удалить</td>
-                                            <td><select id="status${data.id}" name="status[]" >
-                                                <option value='true'>Да</option>
-                                                <option value="false">Нет</option>
-                                            </select></td>
-                                        </tr>
-                                      </table>
-                                      <button onclick="">Update</button>
-                                      <button onclick="deleteData(${data.id})">Delete</button>`)
-        parent.appendChild(elementCont)
-    }
+    async dataOutput(data, model, name, updateData) {
+        let contentItem      = document.getElementById('content-item')
+        let accordion        = document.createElement('button')
+        let accordionContent = document.createElement('div')
+
+        if(data.login) name = data.login, updateData = 'Пароль'
+        else name = data.name, updateData = 'Имя'
+
+        accordion.className = 'accordion'
+        accordion.id = 'accordion'
+        accordion.onclick = addData.accordionDown
+        accordion.innerHTML = `<table>
+                        <tr>
+                            <td>id</td>
+                            <td><input disabled value="${data.id}"></td>
+                            <td>Логин/Имя</td>
+                            <td><input disabled value="${name}"></td>
+                            <td>Удален</td>
+                            <td><input disabled value="${data.isDeleted}"></td>
+                            <td>Модель</td>
+                            <td><input disabled id="model" value="${model}"></td>
+                        </tr>
+                        <tr>
+                            <td>Создан</td>
+                            <td><input disabled value="${data.createdAt}"></td>
+                            <td>Изменен</td>
+                            <td><input disabled value="${data.updatedAt}"></td>
+                            <td>SchoolId</td>
+                            <td><input disabled value="${data.SchoolId}"></td>
+                        </tr>
+                    </table>`
+
+        accordionContent.className = 'panel'
+        accordionContent.innerHTML = `
+                                       <table>
+                                           <tr>
+                                               <td>${updateData}</td>
+                                               <td><input id="input-data${data.id}" value=""></td>
+                                           </tr>
+                                           <tr>
+                                                <td colspan="2" >
+                                                    <button onclick="addData.updateData(${data.id})">Обновить</button>
+                                                </td>
+                                           </tr>
+                                       </table>`
+        contentItem.appendChild(accordion)
+        contentItem.appendChild(accordionContent)
+    },
+
 }
 
-const table = {
+const models = {
     'User'    : new User(),
     'Teacher' : new Teacher(),
     'Class'   : new Class(),
